4cacaac148215ba00ae34a491342b115e3a16a2f
Pawan Chaukiyal <pawanchaukiyal@gmail.com>
2026-02-09 20:47:01 +0530

    STEP 3: Initial NestJS setup and project structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,10 @@
 import { Module } from '@nestjs/common';
-// import { AppController } from './app.controller';
-// import { AppService } from './app.service';
+
 import { IngestionModule } from './ingestion/ingestion.module';
 import { AnalyticsModule } from './analytics/analytics.module';
 import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [IngestionModule, AnalyticsModule, DatabaseModule],
-  // controllers: [AppController],
-  // providers: [AppService],
+  imports: [IngestionModule, AnalyticsModule, DatabaseModule]
 })
 export class AppModule {}
